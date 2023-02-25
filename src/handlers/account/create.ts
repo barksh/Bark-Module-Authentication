1feb550e758eff58e4da7d0134746813d601d32f
wmxpy <wm@mengw.io>
2023-02-25 00:57:39 -0600

    add env test

--- src/handlers/account/create.ts
+++ src/handlers/account/create.ts
@@ -12,6 +12,7 @@ export const createAccountHandler = async (event: any): Promise<any> => {
             {
                 message: "Go Serverless v3.0! Your function executed successfully!",
                 input: event,
+                environment: process.env,
             },
             null,
             2
