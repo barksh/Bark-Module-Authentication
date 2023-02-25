7c99677f5e8457793a09af39f35224ff62cc3da3
wmxpy <wm@mengw.io>
2023-02-24 20:48:14 -0600

    add initial serverless structure

---
+++ src/handlers/create-account.ts
@@ -0,0 +1,13 @@
+export const createAccountHandler = async (event: any): Promise<any> => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless v3.0! Your function executed successfully!",
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
+};
